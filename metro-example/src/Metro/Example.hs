85d00d0f3805665a41fdc2427b92926d9cb21599
Lupino <lmjubuntu@gmail.com>
2019-12-18 21:47:12 +0800

    merge Metro.Example and Metro.ExampleUDP

--- metro-example/src/Metro/Example.hs
+++ metro-example/src/Metro/Example.hs
@@ -1,36 +1,41 @@
-{-# LANGUAGE FlexibleContexts  #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Metro.Example
   ( ServerConfig
   , startMetroServer
-  , ClientConfig
   , startMetroClient
   ) where
 
-import           Control.Monad             (void)
-import           Data.Aeson                (FromJSON, parseJSON, withObject,
-                                            (.!=), (.:), (.:?))
-import           Data.Default.Class        (def)
-import           Metro                     (NodeMode (Multi), initConnEnv,
-                                            runConnT)
-import           Metro.Conn                (close, receive, send)
-import           Metro.Example.Device      (initDeviceEnv, sessionGen,
-                                            sessionHandler, startDeviceT)
-import           Metro.Example.Types       (Command (..), Packet (..))
-import           Metro.Example.Web         (startWeb)
-import           Metro.Servable            (getNodeEnvList, initServerEnv,
-                                            startServer)
-import           Metro.TCP                 (tcpConfig)
-import           Metro.Transport.Socket    (socketUri)
-import           System.IO                 (stderr)
-import           System.Log                (Priority (..))
-import           System.Log.Formatter      (simpleLogFormatter)
-import           System.Log.Handler        (setFormatter)
-import           System.Log.Handler.Simple (streamHandler)
-import           System.Log.Logger
-import           UnliftIO.Concurrent       (forkIO)
+import           Control.Monad          (forever, void)
+import           Data.Aeson             (FromJSON, parseJSON, withObject, (.!=),
+                                         (.:), (.:?))
+import           Data.ByteString        (ByteString)
+import           Data.Default.Class     (def)
+import           Data.List              (isPrefixOf)
+import           Data.Word              (Word16)
+import           Metro                  (NodeMode (..), request, withSessionT)
+import           Metro.Conn             (close, initConnEnv, receive, runConnT)
+import           Metro.Example.Device   (DeviceEnv, initDeviceEnv, runDeviceT,
+                                         sessionGen, sessionHandler,
+                                         startDeviceT)
+import           Metro.Example.Types    (Command (..), Packet (..))
+import           Metro.Example.Web      (startWeb)
+import           Metro.Servable         (Servable (STP, ServConfig), ServerEnv,
+                                         getNodeEnvList, initServerEnv,
+                                         runServerT, startServer, stopServerT)
+import           Metro.Session          (send)
+import           Metro.TCP              (tcpConfig)
+import           Metro.Transport        (Transport, TransportConfig)
+import           Metro.Transport.Debug  (DebugMode (..), debugConfig)
+import           Metro.Transport.Socket (socketUri)
+import           Metro.UDP              (newClient, udpConfig)
+import           Metro.Utils            (setupLog)
+import           System.Log             (Priority (..))
+import           UnliftIO               (liftIO)
+import           UnliftIO.Concurrent    (forkIO, threadDelay)
 
 data ServerConfig = ServerConfig
   { webHost   :: String
@@ -51,39 +56,66 @@ instance FromJSON ServerConfig where
     logLevel  <- read <$> o .:? "log_level" .!= "ERROR"
     return ServerConfig{..}
 
-startMetroServer :: ServerConfig -> IO ()
-startMetroServer ServerConfig {..} = do
-  removeAllHandlers
-  handle <- streamHandler stderr logLevel >>= \lh -> return $
-          setFormatter lh (simpleLogFormatter "[$time : $loggername : $prio] $msg")
-  updateGlobalLogger rootLoggerName (addHandler handle . setLevel logLevel)
+type ExampleEnv serv = ServerEnv serv () ByteString Word16 Packet
 
+newMetroServer
+  :: (Servable serv, Transport tp)
+  => (TransportConfig (STP serv) -> TransportConfig tp)
+  -> NodeMode -> ServConfig serv -> Int -> Int -> IO (ExampleEnv serv tp)
+newMetroServer mk mode config keepalive sessTout = do
   gen <- sessionGen
-  sEnv <- initServerEnv Multi "Example" (tcpConfig sockPort) (fromIntegral keepalive) (fromIntegral sessTout) gen $ \_ connEnv -> do
+  sEnv <- initServerEnv mode "Example" config (fromIntegral keepalive) (fromIntegral sessTout) gen $ \_ connEnv -> do
     cmd <- packetCmd <$> runConnT connEnv receive
     case cmd of
       Data nid -> return $ Just (nid, ())
       _        -> do
         runConnT connEnv close
         return Nothing
-  void $ forkIO $ startServer sEnv id sessionHandler
-  startWeb (getNodeEnvList sEnv) webHost webPort
+  void $ forkIO $ startServer sEnv mk sessionHandler
+  return sEnv
 
-newtype ClientConfig = ClientConfig
-  { cSockPort :: String
-  }
+startMetroServer :: ServerConfig -> IO ()
+startMetroServer ServerConfig {..} = do
+  setupLog logLevel
+  if "udp" `isPrefixOf` sockPort then do
+    sEnv <- newMetroServer (debugConfig "Example" Raw) Multi (udpConfig sockPort) keepalive sessTout
+    startWeb (getNodeEnvList sEnv) webHost webPort
+  else do
+    sEnv <- newMetroServer (debugConfig "Example" Raw) Multi (tcpConfig sockPort) keepalive sessTout
+    startWeb (getNodeEnvList sEnv) webHost webPort
 
-instance FromJSON ClientConfig where
-  parseJSON = withObject "ClientConfig" $ \o -> do
-    cSockPort     <- o .: "socket"
-    return ClientConfig{..}
+startMetroClient :: ServerConfig -> IO ()
+startMetroClient ServerConfig {..} = do
+  setupLog logLevel
+  if "udp" `isPrefixOf` sockPort then do
+    sEnv <- newMetroServer (debugConfig "ExampleClient" Raw) Multi (udpConfig "udp://:0") keepalive sessTout
+    env0 <- runServerT sEnv $ newClient (debugConfig "ExampleClient" Raw) sockPort "client" () sessionHandler
+    case env0 of
+      Nothing   -> do
+        putStrLn "error"
+        runServerT sEnv stopServerT
+      Just env1 -> runAction env1
+  else do
+    connEnv <- initConnEnv (debugConfig "ExampleClient" Raw $ socketUri sockPort)
+    env0 <- initDeviceEnv connEnv "metro-client"
+    void . forkIO $ startDeviceT env0
+    runAction env0
 
-startMetroClient :: ClientConfig -> IO ()
-startMetroClient ClientConfig {..} = do
-  connEnv <- initConnEnv (socketUri cSockPort)
-  runConnT connEnv $ send $ def
+runAction :: (Transport tp) => DeviceEnv tp -> IO ()
+runAction env0 = runDeviceT env0 $ do
+  withSessionT Nothing $ send $ def
     { packetId = 0
     , packetCmd = Data "metro-client"
     }
-  env0 <- initDeviceEnv connEnv "metro-client"
-  startDeviceT env0
+
+  forever $ do
+    pkt <- request Nothing $ def
+      { packetId = 0
+      , packetCmd = Data "metro-client"
+      }
+
+    case pkt of
+      Nothing            -> liftIO $ putStrLn "error: metro-client"
+      Just (p :: Packet) -> liftIO $ print p
+
+    threadDelay 10000000
