d17e02e863ff117bf255f6abdf844389a7d5007c
Lupino <lmjubuntu@gmail.com>
2020-01-02 16:07:25 +0800

    metro-transport-crypto: all method support

--- metro-transport-crypto/src/Metro/TP/Crypto.hs
+++ metro-transport-crypto/src/Metro/TP/Crypto.hs
@@ -5,10 +5,16 @@
 module Metro.TP.Crypto
   ( Crypto
   , crypto
+  , crypto_
   , CryptoMethod (..)
+  , methodEcb
+  , methodCbc
+  , methodCfb
+  , methodCtr
   ) where
 
-import           Crypto.Cipher.Types  (BlockCipher (..))
+import           Control.Monad        (when)
+import           Crypto.Cipher.Types  (BlockCipher (..), IV (..), ivAdd, nullIV)
 import           Data.Binary          (Binary (..), decode, encode)
 import           Data.Binary.Get      (getByteString, getWord32be)
 import           Data.Binary.Put      (putByteString, putWord32be)
@@ -56,37 +62,89 @@ makeBlock bSize msg = Block size $ fixedLengthR fixedSize msg
 getMsg :: Block -> ByteString
 getMsg Block {..} = B.take msgSize encData
 
-prepareBlock :: BlockCipher cipher => (cipher -> ByteString -> ByteString) -> cipher -> Block -> Block
-prepareBlock f c b = b { encData = f c (encData b) }
+prepareBlock
+  :: BlockCipher cipher
+  => (cipher -> IV cipher -> ByteString -> ByteString)
+  -> cipher -> IV cipher -> Block -> Block
+prepareBlock f c iv b = b { encData = f c iv (encData b) }
 
 data CryptoMethod cipher = CryptoMethod
-  { encrypt :: cipher -> ByteString -> ByteString
-  , decrypt :: cipher -> ByteString -> ByteString
+  { encrypt :: cipher -> IV cipher -> ByteString -> ByteString
+  , decrypt :: cipher -> IV cipher -> ByteString -> ByteString
+  , needIV  :: Bool
   }
 
-data Crypto cipher tp = Crypto (TVar ByteString) (CryptoMethod cipher) cipher tp
+data Crypto cipher tp = Crypto
+  { readBuffer   :: TVar ByteString
+  , cryptoMethod :: CryptoMethod cipher
+  , readIV       :: TVar (IV cipher)
+  , writeIV      :: TVar (IV cipher)
+  , cipher       :: cipher
+  , tp           :: tp
+  }
 
 instance (Transport tp, BlockCipher cipher) => Transport (Crypto cipher tp) where
-  data TransportConfig (Crypto cipher tp) = CryptoConfig (CryptoMethod cipher) cipher (TransportConfig tp)
-  newTransport (CryptoConfig method cipher config) = do
-    buf <- newTVarIO empty
-    Crypto buf method cipher <$> newTransport config
-  recvData (Crypto buf method cipher tp) _ = do
+  data TransportConfig (Crypto cipher tp) =
+    CryptoConfig (CryptoMethod cipher) cipher (IV cipher) (TransportConfig tp)
+  newTransport (CryptoConfig cryptoMethod cipher iv config) = do
+    readBuffer <- newTVarIO empty
+    tp <- newTransport config
+    readIV  <- newTVarIO iv
+    writeIV <- newTVarIO iv
+    return Crypto {..}
+  recvData (Crypto buf method ivr _ cipher tp) _ = do
     hbs <- recvEnough buf tp 4
+    iv <- readTVarIO ivr
     case decode (fromStrict hbs) of
-      BlockLength len ->
-        getMsg
-          . prepareBlock (decrypt method) cipher
+      BlockLength len -> do
+        bs <- getMsg
+          . prepareBlock (decrypt method) cipher iv
           . decode
           . fromStrict
           . (hbs <>) <$> recvEnough buf tp len
-  sendData (Crypto _ method cipher tp) =
+
+        when (needIV method) $
+          atomically $ writeTVar ivr (ivAdd iv (B.length bs))
+
+        return bs
+  sendData (Crypto _ method _ ivw cipher tp) bs = do
+    iv <- readTVarIO ivw
+
+    when (needIV method) $
+      atomically $ writeTVar ivw (ivAdd iv (B.length bs))
+
     sendData tp
       . toStrict
       . encode
-      . prepareBlock (encrypt method) cipher
-      . makeBlock (blockSize cipher)
-  closeTransport (Crypto _ _ _ tp) = closeTransport tp
+      . prepareBlock (encrypt method) cipher iv
+      $ makeBlock (blockSize cipher) bs
+  closeTransport (Crypto _ _ _ _ _ tp) = closeTransport tp
 
-crypto :: CryptoMethod cipher -> cipher -> TransportConfig tp -> TransportConfig (Crypto cipher tp)
-crypto = CryptoConfig
+crypto
+  :: BlockCipher cipher
+  => CryptoMethod cipher
+  -> cipher
+  -> TransportConfig tp
+  -> TransportConfig (Crypto cipher tp)
+crypto method cipher = crypto_ method cipher nullIV
+crypto_
+  :: BlockCipher cipher
+  => CryptoMethod cipher
+  -> cipher
+  -> IV cipher
+  -> TransportConfig tp
+  -> TransportConfig (Crypto cipher tp)
+crypto_ = CryptoConfig
+
+methodEcb :: BlockCipher cipher => CryptoMethod cipher
+methodEcb = CryptoMethod (ignoreIV ecbEncrypt) (ignoreIV ecbDecrypt) False
+  where ignoreIV f c _ = f c
+
+methodCbc :: BlockCipher cipher => CryptoMethod cipher
+methodCbc = CryptoMethod cbcEncrypt cbcDecrypt True
+
+methodCfb :: BlockCipher cipher => CryptoMethod cipher
+methodCfb = CryptoMethod cfbEncrypt cfbDecrypt  True
+
+methodCtr :: BlockCipher cipher => CryptoMethod cipher
+methodCtr = CryptoMethod ctrCombine ctrCombine True
